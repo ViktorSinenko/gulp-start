ba96f8bd422e2299c43b9da89c1f48bfe625931e
ViktorSinenko <svfasad@icloud.com>
2023-09-27 12:23:36 +0300

    gulp-start

---
+++ app/js/main.js
@@ -0,0 +1,4 @@
+// const box = document.querySelector('.box');
+// const btn = document.querySelector('.button');
+// const menu = document.querySelector('.menu');
+// const men = document.querySelector('.men');
